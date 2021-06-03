19eda13ef0284c2f5bb5de6ad938d6b83665da67
mesmerxx <minlee@cuc.edu.cn>
2021-06-03 23:50:36 +0800

    :tada:

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,22 +1,34 @@
+import { getSidebar } from "../../scripts/utils";
 module.exports = {
-  title: "前端笔记",
-  description: "To be a real front-end programmer ",
-  nav: [
-    {
-      text: "CS",
-      link: "/cs/",
-    },
-    {
-      text: "JS",
-      link: "/js/",
-    },
-    {
-      text: "CSS",
-      link: "/css",
-    },
-    {
-      text: "react",
-      link: "/react/",
-    },
-  ],
+  title: "fe-note",
+  description: "成为一名真正的前端程序员之旅",
+  themeConfig: {
+    repo: "MleMoe/fe-note",
+    docsBranch: "main",
+    docsDir: "docs",
+
+    editLinks: true,
+    editLinkText: "帮助改善文档！",
+    lastUpdated: "上次更新",
+    nav: [
+      {
+        text: "CS通识",
+        link: "/cs/",
+      },
+      {
+        text: "JS",
+        link: "/js/",
+      },
+      {
+        text: "CSS",
+        link: "/css",
+      },
+      {
+        text: "REACT",
+        link: "/react/",
+      },
+    ],
+
+    sidebar: null,
+  },
 };
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,15 @@
-# Hello VuePress
+---
+home: true
+heroText: Mia's FE Notes
+actionText: 开始旅行
+actionLink: /guide/
+features:
+  - title: 笔记由来
+    details: 一直自称是虚假的前端程序员的我，最终选定以后主要用 JS 编程
+  - title: 这里记录📝
+    details: 前端知识点（可能会写成面试大全）& 实习经验沉淀（阿里的前辈真的很🐂）
+  - title: 最终
+    details: 我会成为一名真正的前端程序员的(坚定)
+
+footer: MIT Licensed | Copyright © 2021-present MleMoe(MIA)
+---
