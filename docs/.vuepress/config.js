c0af98c493c22d56ff7dc787103b91a38317f21d
mingshi <mingshi.lm@alibaba-inc.com>
2021-06-03 18:35:09 +0800

    :tada:

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  title: "前端笔记",
+  description: "To be a real front-end programmer ",
+  nav: [
+    {
+      text: "CS",
+      link: "/cs/",
+    },
+    {
+      text: "JS",
+      link: "/js/",
+    },
+    {
+      text: "CSS",
+      link: "/css",
+    },
+    {
+      text: "react",
+      link: "/react/",
+    },
+  ],
+};
