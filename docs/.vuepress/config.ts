5c1ef632b966cd1bf0ccb69ef2afab51c2d2a64d
mingshi <mingshi.lm@alibaba-inc.com>
2021-07-11 00:09:01 +0800

    :zap: update to vuepress2

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,52 @@
+import { defineUserConfig } from "vuepress";
+import type { DefaultThemeOptions } from "vuepress";
+
+export default defineUserConfig<DefaultThemeOptions>({
+  title: 'note',
+  description: 'Mia 的前端之旅',
+  bundler: '@vuepress/vite',
+
+  head: [
+    ["link", { rel: "icon", href: "/logo.png" }],
+  ],
+
+  locales: {
+    "/": {
+      lang: "zh-CN",
+      description:
+        "Mia's fe-notes",
+    },
+    },
+  themeConfig: {
+    logo:"/logo.png",
+    repo: 'MleMoe/fe-note',
+    docsBranch: 'main',
+    docsDir: 'docs',
+
+    editLinks: true,
+    editLinkText: '帮助改善文档！',
+    lastUpdatedText: '上次更新',
+
+    head: [
+      ['link', { rel: 'icon', href: '/logo.png', type: 'image/png' }],
+      ['meta', { name: 'author', content: 'MleMoe-MIA' }],
+    ],
+
+    // displayAllHeaders: true,
+    locales: {
+      "/": {
+        darkMode: true,
+        selectLanguageName: "简体中文",
+        lastUpdatedText: "上次更新",
+        editLinkText: "帮助改善此页面",
+      
+        sidebar:{
+          '/travel/': ['', 'todo-react.md'],
+          '/js/': ['', 'object.md'],
+        },
+      },
+    }
+
+    
+  },
+});
--- docs/README.md
+++ docs/README.md
@@ -1,18 +1,20 @@
 ---
-layout: HomeLayout
+title: home
 home: true
 heroImage: /logo.png
-heroAlt: Logo image
 heroText: Mia's FE Notes
-actionText: 开始旅行
-actionLink: /travel/
+tagline: Mia 的前端之旅
+
+actions:
+  - text: 开始旅行
+    link: /travel/
 features:
   - title: 笔记由来
-    details: 一直自称虚假前端程序员的我，最终选定主要用 JS/TS 编程
+    details: 自称虚假前端程序员的我，最终选定主用 JS/TS 编程
   - title: 这里记录📝
-    details: 前端知识点 + 前端打工知识收获
+    details: 前端知识点 + 前端打工生活
   - title: 那么就开始吧
-    details: Go!
+    details: Go Mia !
 
 footer: MIT Licensed | Copyright © 2021-present MleMoe(MIA)
 ---
